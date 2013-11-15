b4bf8597c565fd351d35c9233e143a5886860738
ag <ag@kiben.net>
2013-11-15 22:45:19 +0000

    rrd

--- test.py
+++ test.py
@@ -15,8 +15,8 @@ def timestamp():
 
 def getTemp(sensor):
     raw = open(path+sensor+"/w1_slave", "r").read()
-    current_temp = str(float(raw.split("t=")[-1])/1000)
-    print "%s  : %s C" %(sensor, current_temp)
+    current_temp = float(raw.split("t=")[-1])/1000
+    print "%s  : %s C" %(sensor, str(current_temp))
     rrdtool.update("/run/shm/"+sensor+".rrd", "N:%f" % current_temp)
     return raw
 
