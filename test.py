7bf11c39c21c728bf80bd57d422cf24257200628
ag <ag@kiben.net>
2013-11-15 21:39:09 +0000

    PP

--- test.py
+++ test.py
@@ -7,10 +7,6 @@ import datetime
 path = "/sys/bus/w1/devices/"
 tempSensor = { 'beagle' : '10-0008029674ee', 'out' : '10-000802964c0d', 'cylinder' : '10-000802961f0d', 'stove' : '10-00080296978d' }
 
-beagleTmp = "/sys/bus/w1/devices/10-0008029674ee/w1_slave"
-outTmp    = "/sys/bus/w1/devices/10-000802964c0d/w1_slave"
-stoveTmp  = "/sys/bus/w1/devices/10-000802961f0d/w1_slave"
-waterTmp  = "/sys/bus/w1/devices/10-000802961f0d/w1_slave"
 
 def timestamp():
     timestamp = json.dumps(datetime.datetime.now().isoformat()).split('"')[1]
@@ -30,7 +26,7 @@ while True:
         address = tempSensor[k]
         getTemp(address)
         time.sleep(2)
-        print "----------"
+    print "----------"
     #print "Temperature is %s %s degrees" %(str(float(raw.split("t=")[-1])/1000),str(float(raw1.split("t=")[-1])/1000))
     #doc = {"_id": timestamp() , "temp1" : str(float(raw.split("t=")[-1])/1000), "temp2" : str(float(raw1.split("t=")[-1])/1000)}
     #db.save(doc)
