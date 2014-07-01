428a70292f04650215011d1c35b72571522ed871
ag <ag@kiben.net>
2014-07-01 12:07:46 +0100

    ssh key manage

--- provision/boat.pp
+++ provision/boat.pp
@@ -13,6 +13,7 @@ class storage (
  $device  = '/dev/sdb1' )
 {
   file { $datadir : ensure => directory }
+
   mount { $datadir : 
    device  => $device,
    ensure  => mounted,
@@ -27,18 +28,33 @@ class wireless (
   $ssid = 'boat',
   $channel = '8')
 {
-  $packages = ['hostapd', 'firmware-atheros']
-  package {$packages : ensure => present }
-
-  service { hostapd : ensure  => running, require => Package[$packages] }
-
-  file {'/etc/hostapd/hostapd.conf' : 
-   ensure  => present,
-   content => template("teplates/hostapd/hostapd.conf"),
-   notify  => Service['hostap'],
-   }
+  class ap {
+    
+    $packages = ['hostapd', 'firmware-atheros']
+    package {$packages : ensure => present }
+    
+    service { hostapd : ensure  => running, require => Package[$packages] }
+    
+    file {'/etc/hostapd/hostapd.conf' : 
+      ensure  => present,
+      content => template("teplates/hostapd/hostapd.conf"),
+      notify  => Service['hostap'],
+    }
+  }
+  class client {
+    ###
+  }
+    
 }
 
+class users {
+  ssh_authorized_key { 'mykey' :
+    ensure => present,
+    user   => root,
+    type   => 'ssh-rsa',
+    key    => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAlbfVzsjc6/n+nN6dTpihPNLJNVDgM1g5E+OcAj9ZSfS7eivAAzW5mEo0XrwBfeIGNxEOP9IiCXRyMTcQjilC1H0qQb3j6k3fFX+s+35bqAWuxWXbCLHHumgYAxHhEJxORl9L5ZKPWg125OKuk5UJmV6D2qE0SncbwymzAMqjFcxlhp6s3I+uvfa7Hp4s1ynaZYxW89vSMG1cG3j6Dv8+dsdHJH1mvkVP5NgyczC3d2j9u09TMqB/ugZJ5b8W/PGqvfeajFgROOVrQQNA/QrbY4SbtQsSy6sSjuQLHSjlc7sZ9E5XIwTeLVlZWfpMihAfBV7gicrYgcDi78uS8xSDCw==',
+
+}
 class network {
   #
 }
