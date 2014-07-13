d5b9b150532a3c35e90658f701c01384c3c73bfc
ag <ag@kiben.net>
2014-07-13 11:49:07 +0100

    added dummy temp and water methods

--- sensors.rb
+++ sensors.rb
@@ -16,11 +16,13 @@ module Boattr
       @name     = params['name']
       @address  = params['address']
       @@device = ::I2C.create(params['i2cBus'])
+      self.read()
     end
     def read
       @data = []
       # read 20 bytes from slave, convert to decimals, and finaly in 10bit values.
       @adc = @@device.read(address, 0x14, 0x00).unpack('C*').map {|e| e.to_s 10}
+      #slice the 20 byte array into pairs (MSB,LSB) and convert. 
       @adc.each_slice(2) {|a| @@data << a[0].to_i*256+a[1].to_i}
       return @@data
     end
@@ -28,14 +30,18 @@ module Boattr
       @name    = name
       @raw     = @@data[address]
       @volts = @raw * 0.015357
-      return { 'name' => @name,'raw' => @raw ,'volts' => @volts.round(3) }
+      return { 'name' => @name, 'type' => 'voltage', 'raw' => @raw, 'volts' => @volts.round(3) }
     end
     def current(name,address)
       @name    = name
       @raw     = @@data[address]
       @volts   = (@raw*0.004887)
       @amps    = (@volts-2.5)/0.066 
-      return { 'name' => @name,'raw' => @raw ,'volts'=> @volts.round(3) ,'amps' => @amps.round(3)} 
+      return { 'name' => @name, 'type' => 'current', 'raw' => @raw, 'volts'=> @volts.round(3), 'amps' => @amps.round(3)} 
+    end
+    def temperature(name,address)
+    end
+    def waterlevel(name,address)
     end
   end
   class Data  
@@ -67,13 +73,12 @@ brain02 = {
 }
 
 
-foo=Boattr::Sensors.new(brain02)
-p foo.read()
+sensors=Boattr::Sensors.new(brain02)
+#p foo.read()
 
-brain02_sensors=[foo.voltage('bat',1), foo.current('amps',0)]
+brain02_sensors=[sensors.voltage('bat',1), sensors.current('amps',0)]
 
-bar=Boattr::Data.new()
-bar.to_db(brain02_sensors)
+Boattr::Data.new().to_db(brain02_sensors)
 
 
 
