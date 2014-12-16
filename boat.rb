c938f661ea443a0415c8ccf5930816561e34aa56
ag <ag@kiben.net>
2014-12-16 11:46:30 +0000

    split sensors into groups and add some more temp sensors for the boat.

--- boat.rb
+++ boat.rb
@@ -10,29 +10,54 @@ brain01 = {
   'couchdb'     => 'localhost',
   'dashboard'   => 'localhost',
   'graphite'   => '10.70.60.1',
+  'dash_auth'   => 'YOUR_AUTH_TOKEN',
 }
 
 sensors=Boattr::Sensors.new(brain01)
 dataAllowance = Boattr::Data.new(brain01).get_remaining_data('ee') 
-brain01_sensors =
-  [ sensors.current('solar',0,model='acs714',type='src'), 
-    sensors.current('genny',1,model='acs709',type='src'), 
-    sensors.current('lights',2,model='acs714',type='load'),
-    #sensors.current('pumps',3),
-    sensors.current('ring',4,model='acs714',type='load'),
-    sensors.current('fridge',5,model='acs714',type='load'),
-    sensors.voltage('batteries',6),
-    #sensors.waterlevel('tank',7),
-    sensors.temperature('out','10-000802964c0d'),
-    sensors.temperature('in','10-0008029674ee'),
-    sensors.temperature('cylinder','10-000802961f0d'),
-    sensors.temperature('stove','10-00080296978d'),
-    sensors.temperature('canal','28-000004ee99a8'),
-    dataAllowance,
+
+@current_sensors = [ 
+                   sensors.current('solar',0,model='acs714',type='src'), 
+                   sensors.current('genny',1,model='acs709',type='src'), 
+                   sensors.current('lights',2,model='acs714',type='load'),
+                   #sensors.current('pumps',3),
+                   sensors.current('ring',4,model='acs714',type='load'),
+                   sensors.current('fridge',5,model='acs714',type='load'),
+                  ]
+@temp_sensors = [ sensors.temperature('in','10-0008029674ee'),
+                  sensors.temperature('out','10-000802964c0d'),
+                  sensors.temperature('cylinder','10-000802961f0d'),
+                  sensors.temperature('stove','10-00080296978d'),
+                  sensors.temperature('canal','28-000004ee99a8'), 
+                  sensors.temperature('bath','28-000005661bfa'), 
+                  #sensors.temperature('one','28-0000056b2c1a'), 
+                  #sensors.temperature('two','28-000005657712'), 
+                  sensors.temperature('bed','28-00000566f3b8'), 
+                ]
+
+@brain01_sensors = [ 
+                   sensors.voltage('batteries',6),
+                   #sensors.waterlevel('tank',7),
+                   dataAllowance,
 ]
 
+@brain01_sensors.concat(@current_sensors)
+@brain01_sensors.concat(@temp_sensors)
 
-Boattr::Data.new(brain01).to_db(brain01_sensors)
-Boattr::Data.new(brain01).to_graphite(brain01_sensors)
-Boattr::Data.new(brain01).to_dashboard(brain01_sensors)
+Boattr::Data.new(brain01).to_db(@brain01_sensors)
+Boattr::Data.new(brain01).to_graphite(@brain01_sensors)
 
+@hours = 24
+dashing = Boattr::Data.new(brain01)
+dashing.to_dashboard(@brain01_sensors)
+
+
+
+@boat_amph = dashing.amphours(@brain01_sensors,@hours)
+@balance  = dashing.amphourBalance(@boat_amph)
+
+@balance.concat(@boat_amph)
+
+#Boattr::Data.new(brain01).new_to_dashboard(@balance,'amphours')
+Boattr::Data.new(brain01).new_to_dashboard(@current_sensors,'amps')
+Boattr::Data.new(brain01).new_to_dashboard(@temp_sensors,'temps')
