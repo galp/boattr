a5a517b967b2f207cb882fac89321bd8530bffd9
ag <ag@kiben.net>
2015-01-07 19:47:34 +0000

    remove some unused puppet code

--- provision/modules/couchdb/manifests/cron.pp
+++
@@ -1,8 +0,0 @@
-class cron {
-  cron { compact_db:
-    command => "/usr/bin/curl  -H "Content-Type: application/json" -X POST  http://${db_host}:5984/sensors/_compact",
-    user    => root,
-    hour    => 2,
-    minute  => 0,
-  }
-}
