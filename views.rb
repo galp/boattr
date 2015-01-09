20ffe1f4035920957e52e370312a203fdb6a8e50
ag <ag@kiben.net>
2015-01-09 00:13:50 +0000

    tidying up views

--- views.rb
+++ views.rb
@@ -1,14 +1,10 @@
 require './boattr.rb'
 
-config = Boattr::Config.read('/root/boattr/config.yml')
 
-v = Boattr::Data.new(config)
-@sensor_data = []
-config['sensors'].each() { |k,v| v.each() { |x| @sensor_data << x[1].merge!({'type' => k, 'name' => x[0] }) }}
+@sensor_data = Boattr::Config.sensors
+v = Boattr::Data.new(@config)
 
-
-
+p @sensor_data
 v.create_views(@sensor_data,'current')
-
 v.create_views(@sensor_data,'temp')
 v.create_views(@sensor_data,'voltage')
