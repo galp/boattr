4a5eec62780aeecc209fd66dabb4393465d0ba63
ag <ag@kiben.net>
2015-01-12 10:09:36 +0000

    small fix after recent changes

--- amphours.rb
+++ amphours.rb
@@ -1,13 +1,16 @@
 require '/root/boattr/boattr.rb'
 config = Boattr::Config.read('/root/boattr/config.yml')
+active_sensors = Boattr::Config.sensors(config)
 
-current_sensors = config['sensors']['current']
+
+
 @week  = 720 # 1 week
 @hours = 24
 a = Boattr::Data.new(config)
 
-@boat_amphours = a.amphours(current_sensor, @hours)
+@boat_amphours = a.amphours(active_sensors, @hours)
 @balance       = a.amphour_balance(@boat_amphours)
 @balance.concat(@boat_amphours)
+
 d = Boattr::Dashing.new(config)
 d.list_to_dashboard(@balance, 'amphours')
