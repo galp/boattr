2a290ebc78fddb860ff202ca09b880df0655bda7
ag <ag@kiben.net>
2015-03-18 13:22:48 +0000

    Adding zlib-dev dependency for ruby and friends

--- provision/modules/boattr/manifests/packages.pp
+++ provision/modules/boattr/manifests/packages.pp
@@ -5,7 +5,7 @@ class boattr::packages (
   require boattr::apt
 
   $packagelist     = [ 'screen', 'mosh','iptables','curl']
-  $rubypackagelist = [ 'ruby','bundler','ruby-dev','rubygems','build-essential','libc6-dev']
+  $rubypackagelist = [ 'ruby','bundler','ruby-dev','zlib1g-dev','rubygems','build-essential','libc6-dev']
   $devpackagelist  = ['i2c-tools','emacs24-nox', 'puppet-el']
 
   apt::force { $rubypackagelist: release => 'testing' }
