0e6c44cb61f9c1d968ee7b200d5e348437b140bc
ag <ag@kiben.net>
2015-04-23 13:13:27 +0100

    simplifying default.pp

--- provision/default.pp
+++ provision/default.pp
@@ -4,11 +4,4 @@ node default {
   -> class { 'boattr': } -> class { 'boattr::dashing': } -> class { 'boattr::couchdb': }
   -> class { 'boattr::udev': phone_mac => $phone_mac, wifi_mac => $wifi_mac }
   class { 'boattr::dnsmasq': }
- 
-
-
-
-}
-node boattr-vagrant inherits default {
-
 }
