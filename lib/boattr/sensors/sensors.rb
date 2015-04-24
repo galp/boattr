01845c0ddab72c75519edf0607fcf05b2564d443
ag <ag@kiben.net>
2015-04-24 16:00:18 +0000

    an update to arduino adc work

--- lib/boattr/sensors/sensors.rb
+++ lib/boattr/sensors/sensors.rb
@@ -1,9 +1,18 @@
 
 module Boattr
   class Sensors
-    attr_reader :i2c_adc, 
+    attr_accessor :data
+    attr_reader :i2c_adc
     def initialize(params)
       @i2c_adc  = params['i2c']['i2cAdc']
+      @@data = {}
+      i2c_adc.each do |k,v|
+        next if v['disabled']
+        if v['type'] == 'arduino'
+          @@data[k] = Boattr::Sensors::Arduino.new(v).read()
+        end
+      end
     end
   end
 end
+
