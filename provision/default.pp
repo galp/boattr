b0c5c9806538fe7cdabfeb595e237ec147ebead7
ag <ag@kiben.net>
2015-05-13 17:07:19 +0000

    remove devel param as in not doing anything

--- provision/default.pp
+++ provision/default.pp
@@ -1,6 +1,6 @@
 node default {
 
-  class { 'boattr::packages':  devel => false } -> class { 'boattr::ntp': } -> class { 'boattr::users': }
+  class { 'boattr::packages': } -> class { 'boattr::ntp': } -> class { 'boattr::users': }
   -> class { 'boattr': } -> class { 'boattr::dashing': } -> class { 'boattr::couchdb': }
   class { 'boattr::dnsmasq': }
 }
