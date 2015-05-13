bf760f084bd6871bf6e8ebc5240ddfc0eadac6dd
ag <ag@kiben.net>
2015-05-13 15:59:31 +0100

    pretty message

--- lib/boattr/sensors/arduino.rb
+++ lib/boattr/sensors/arduino.rb
@@ -10,11 +10,12 @@ module Boattr
       def read()
         @bytes = 4
         begin
+          puts "* getting data from i2c device at address #{i2c_address}"
           @i2c_device.write(i2c_address, 0x00)
           @data        = @i2c_device.read(i2c_address, 4).unpack('C*').map { |e| e.to_s 10 }
           @i2c_device.disable
         rescue
-          puts "i2c device at address #{i2c_address} not responding"
+          puts "* i2c device at address #{i2c_address} not responding"
           return
         end
         return @data
