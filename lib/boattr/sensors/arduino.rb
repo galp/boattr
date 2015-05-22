6cf6f48e38eed6f572df5b448459093b5692a878
ag <ag@kiben.net>
2015-05-22 17:00:51 +0000

    arduino pins are configurable

--- lib/boattr/sensors/arduino.rb
+++ lib/boattr/sensors/arduino.rb
@@ -6,18 +6,19 @@ module Boattr
       def initialize(params)
         @i2c_device  = ::Beaglebone::I2CDevice.new(params['dev'].to_sym)
         @i2c_address = params['address']   #0x20
+        @i2c_bytes        = params['pins'] || @i2c_bytes = 4
       end
       def read()
-        @bytes = 4
         begin
           @i2c_device.write(i2c_address, 0x00)
-          @data        = @i2c_device.read(i2c_address, 4).unpack('C*').map { |e| e.to_s 10 }
+          @data        = @i2c_device.read(i2c_address, @i2c_bytes).unpack('C*').map { |e| e.to_s 10 }
           puts "* getting data from i2c device at address #{i2c_address}"
           @i2c_device.disable
         rescue
           puts "* i2c device at address #{i2c_address} not responding"
           return
         end
+	p @data
         return @data
       end
     end
