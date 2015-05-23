60ee5f307fd05ba0292c2485a4c171dff229e5a4
ag <ag@kiben.net>
2015-05-23 13:09:15 +0100

    minor fixes in depth code.
    removing hardcoded offset and depth = 0 if read value less than offset.

--- lib/boattr/sensors/depth.rb
+++ lib/boattr/sensors/depth.rb
@@ -1,15 +1,17 @@
 module Boattr
   class Depth < Sensors
-    attr_reader :name, :address
+    attr_reader :name, :address, :offset
     def initialize(name, address)
       @name     = name
       @address  = address
+      @offset   = 50
     end
 
     def read
       return if @@data.nil? || @@data.empty? || @@data[address['adc']].nil?
       @raw     = @@data[address['adc']][address['pin']]
-      @depth  = (@raw.to_i-50) * 0.95 
+      @depth  = (@raw.to_i-@offset) * 0.95
+      @depth  = 0 if @depth < 0
       { 'name' => name, 'type' => 'depth', 'raw' => @raw, 'value' => @depth.round(2) }
     end
   end
